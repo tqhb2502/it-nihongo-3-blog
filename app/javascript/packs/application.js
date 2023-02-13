e559c6b8dd4dfc8a54cb55d53288edae5fda6186
tqhb2502 <tqhb2502@gmail.com>
2023-02-13 15:52:49 +0000

    add dragging elements

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,6 +19,8 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 
+import Sortable from 'sortablejs'
+
 import '../stylesheets/application';
 
 document.addEventListener('turbolinks:load', () => {
@@ -35,10 +37,14 @@ document.addEventListener('turbolinks:load', () => {
   document.addEventListener('click', () => {
     
     if (!event.target.matches('.cancel')) return;
+    event.preventDefault();
     
     let element = event.target.closest('.paragraph-form');
     
     element.classList.add('d-none');
     element.previousElementSibling.classList.remove('d-none');
   });
+  
+  let element = document.getElementById('elements');
+  Sortable.create(elements, { animation: 150 });
 });
